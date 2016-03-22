80f6824225facdc428d1438adcf479c81ce7b3d4
Aires Gonçalves <airesvsg@gmail.com>
2016-03-22 11:17:38 -0300

    regexp

--- lib/endpoints/class-acf-to-rest-api-controller.php
+++ lib/endpoints/class-acf-to-rest-api-controller.php
@@ -24,7 +24,7 @@ if ( ! class_exists( 'ACF_To_REST_API_Controller' ) ) {
 		}
 
 		public function register_routes() {
-			register_rest_route( $this->namespace, "/{$this->type}/(?P<id>\d+)/?", array(
+			register_rest_route( $this->namespace, "/{$this->type}/(?P<id>[\d]+)", array(
 				array(
 					'methods'             => WP_REST_Server::READABLE,
 					'callback'            => array( $this, 'get_item' ),
